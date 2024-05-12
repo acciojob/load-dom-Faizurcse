fa676ad6d846e640695c27a49ab05661863e4cf2
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-05-13 03:48:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  // Create a new paragraph element
+  let body = document.querySelector("body");
+	body.textContent = "DOM load success";
+});
